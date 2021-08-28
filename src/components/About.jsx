aea33e6edf8af8d47b45e9d54a654860dc808bbd
stepheninfanto <infantostephen@gmail.com>
2021-08-28 22:22:00 +0530

    comment excess content in about
     import { Col, Form,Input } from 'reactstrap';

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,13 @@ function About(props) {
     return (
         <div>
       <section id="welcome-section" className="welcome-section "> 
-         {/* <h1 className="text-"> Hi I am Stephen Infanto, <br/>
-         a 24 year old Web developer, and a  Blogger<br/>
-           living in Chennai, India. I am a Web Engineer,currently working with great folks.<br/>
+         
+         <h1 className="text-"> 
+         Hi I am Stephen Infanto,A 24 year old Web developer, and a  Blogger</h1>
+           {/* living in Chennai, India. I am a Web Engineer,currently working with great folks.<br/>
            Just connect with me on LinkedIn. 
-        I am always excited to do business with  great people , lets discuss over coffee.
-       </h1> <br/> <br/> */}
+        I am always excited to do business with  great people , lets discuss over coffee. */}
+            <br/> <br/>
         <h1 id="dev-name1">A web developer who loves to work with </h1><br/> <br/>  
         {tech.map((tec,i)=>{
             return (                
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Form,Input } from 'reactstrap';
+// import { Col, Form,Input } from 'reactstrap';
 
 
 
